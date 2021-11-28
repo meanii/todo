ceb69b06938e19de3aa861061d99900c7f650b71
meanii <anilchauhanxda@gmail.com>
2021-11-28 09:34:29 +0530

    defined routed and make web page more organized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './tasks/create/create.component';
+import { ListComponent } from './tasks/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ListComponent},
+  {path:'create', component: CreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
